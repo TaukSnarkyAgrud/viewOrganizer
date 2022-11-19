49828cd006882612f12412c086fbb80c67346b3d
Andrew <4742510+TaukSnarkyAgrud@users.noreply.github.com>
2022-11-18 20:22:09 -0600

    added object capture

--- viewOrganizer.ps1
+++ viewOrganizer.ps1
@@ -1,1 +1,15 @@
-#
+Add-Type -AssemblyName System.Windows.Forms
+$as = [System.Windows.Forms.Screen]::AllScreens
+
+$displays = $as | ForEach-Object {
+    [PSCustomObject]@{
+    GraphicPort = $_.DeviceName.substring($_.DeviceName.IndexOf('Y')+1, 1)
+    Primary = $_.Primary
+    Resolution = @{
+        Width  = $_.Bounds.Width
+        Height = $_.Bounds.Height
+    }
+    Position = $_.WorkingArea.Location
+}}
+
+$displays
